e7ec825283f4c07ce916ee8d0aa4acabef0794ef
Mrunal <lanurm123@gmail.com>
2022-01-01 18:19:39 +0530

    first commit

---
+++ home/views.py
@@ -0,0 +1,24 @@
+from django.shortcuts import render
+
+
+# Create your views here.
+
+def home(request):
+    return render(request, 'index.html')
+
+def about(request):
+    return HttpResponse('This is about me')
+
+def projects(request):
+    pass
+
+def work(request):
+    pass
+
+def education(request)
+
+def 
+
+
+
+
